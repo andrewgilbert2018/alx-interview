11a387f532bd0d945fe5c4c855071378c8d30083
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-09-28 20:30:59 +0000

    0-main.py

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -5,12 +5,12 @@
 pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
 def print_triangle(triangle):
-        """
-            Print the triangle
-                """
-                    for row in triangle:
-                                print("[{}]".format(",".join([str(x) for x in row])))
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
 
 
-                                if __name__ == "__main__":
-                                        print_triangle(pascal_triangle(5))
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
